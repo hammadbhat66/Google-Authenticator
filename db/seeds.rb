61159b7191c49c0baece94e499e4351b21116eae
Hammad <hammadbhat66@gmail.com>
2019-10-09 11:57:35 +0530

    Modified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-	User.create(id:1,user_type: 'Admin')
+	Role.create(id:1,user_type: 'Admin')
 	Role.create(id:2,user_type: 'Employee')
 	Role.create(id:3,user_type: 'Client')
-	Role.create(email: 'eee@gmail.com',password: '123456',role_id: 1)
+	User.create(email: 'eee@gmail.com',password: '123456',role_id: 1)
